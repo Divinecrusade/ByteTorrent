c3fcbf275d6c487f7df21738902ab4663571d32f
Divinecrusade <gorlovdima@inbox.ru>
2025-11-09 21:03:59 +0300

    feat: Make cmake configuration more agile to source files

--- cmake/ModuleUtils.cmake
+++ cmake/ModuleUtils.cmake
@@ -1,16 +1,27 @@
 function(add_module_library MODULE_NAME)
     add_library(${MODULE_NAME})
-    target_sources(${MODULE_NAME} 
-        PUBLIC FILE_SET CXX_MODULES FILES 
+    
+    file(GLOB MODULE_INTERFACES 
         modules/${MODULE_NAME}.ixx
+        modules/${MODULE_NAME}-*.ixx
     )
+    
+    target_sources(${MODULE_NAME} 
+        PUBLIC FILE_SET CXX_MODULES FILES ${MODULE_INTERFACES}
+    )
+    
+    file(GLOB MODULE_SOURCES src/${MODULE_NAME}/*.cpp)
+    
+    if(MODULE_SOURCES)
+        target_sources(${MODULE_NAME} PRIVATE ${MODULE_SOURCES})
+    endif()
+    
     target_compile_features(${MODULE_NAME} PUBLIC cxx_std_23)
     
     if(MSVC)
         target_link_libraries(${MODULE_NAME} PUBLIC std)
     endif()
 
-    # Optional dependencies
     if(ARGN)
         target_link_libraries(${MODULE_NAME} PUBLIC ${ARGN})
     endif()
--- modules/bencode.ixx
+++
@@ -1,7 +0,0 @@
-export module bencode;
-
-import <string_view>;
-
-export namespace bencode {
-void parse(std::string_view input) {}
-}  // namespace bencode
